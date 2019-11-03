ba990cb143630e756624b09414e43fcf12ee528c
Fede <piacentini08@gmail.com>
2019-11-02 21:44:46 -0300

    Funcionan dos rondas

--- src/main/java/pumba/messages/FinishRoundMessage.java
+++ src/main/java/pumba/messages/FinishRoundMessage.java
@@ -5,28 +5,62 @@ import java.io.IOException;
 import pumba.exceptions.PumbaException;
 import pumba.handlers.GameHandler;
 import pumba.messages.utils.SocketMessage;
+import pumba.models.game.StateReduced;
+import pumba.server.ClientListener;
 
 public class FinishRoundMessage extends SocketMessage
 {
 
+	private StateReduced actualState;
+
+	private static Integer cantPlayers = 0;
+
 	public FinishRoundMessage()
 	{
-		super(true);
+		super(false);
 	}
 
 	@Override
 	public void processResponse(Object object)
 	{
-		try
+
+		cantPlayers++;
+		if (cantPlayers < GameHandler.getPlayers().size())
 		{
-			GameHandler.finishRound();
-			this.setApproved(true);
+			try
+			{
+				synchronized (object)
+				{
+					object.wait();
+				}
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
 		}
-		catch (PumbaException e)
+		else
 		{
-			this.setApproved(false);
-			this.setErrorMessage(e.getMessage());
+			cantPlayers = 0;
+			try
+			{
+				GameHandler.finishRound();
+			}
+			catch (PumbaException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			for (ClientListener client : notCurrentClients())
+			{
+				synchronized (client)
+				{
+					client.notifyAll();
+				}
+			}
 		}
+		this.setApproved(true);
 
 		try
 		{
